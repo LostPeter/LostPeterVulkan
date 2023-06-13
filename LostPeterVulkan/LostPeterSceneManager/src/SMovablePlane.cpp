d4822290eff2c8f2c9306b61a10b991b880243ca
jianweidou <douzi0630@sohu.com>
2023-06-13 22:22:07 +0800

    LostPeterVulkan: Update LostPeterSceneManager

---
+++ LostPeterVulkan/LostPeterSceneManager/src/SMovablePlane.cpp
@@ -0,0 +1,17 @@
+/****************************************************************************
+* LostPeterSceneManager - Copyright (C) 2022 by LostPeter
+* 
+* Author:   LostPeter
+* Time:     2023-06-13
+* Github:   https://github.com/LostPeter/LostPeterVulkan
+* Document: https://www.zhihu.com/people/lostpeter/posts
+*
+* This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
+****************************************************************************/
+
+#include "../include/SMovablePlane.h"
+
+namespace LostPeterSceneManager
+{
+    
+}; //LostPeterSceneManager
