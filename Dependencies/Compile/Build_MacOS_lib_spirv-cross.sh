24bd3ee175dfe896efbd81495d9b545650648b54
jianweidou <douzi0630@sohu.com>
2024-06-08 15:51:50 +0800

    Add Dependency lib: spirv-cross-vulkan-sdk-1.3.283,  Compile Script

---
+++ Dependencies/Compile/Build_MacOS_lib_spirv-cross.sh
@@ -0,0 +1,107 @@
+#****************************************************************************
+# LostPeterVulkan - Copyright (C) 2022 by LostPeter
+# 
+# Author:   LostPeter
+# Time:     2024-06-08
+# Github:   https://github.com/LostPeter/LostPeterVulkan
+# Document: https://www.zhihu.com/people/lostpeter/posts
+#
+# This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
+#****************************************************************************/
+
+debug=${1}
+rebuild=${2}
+
+name=spirv-cross-vulkan-sdk-1.3.283
+
+name_lib_core=libspirv-cross-core
+name_lib_c=libspirv-cross-c
+name_lib_cpp=libspirv-cross-cpp
+name_lib_glsl=libspirv-cross-glsl
+name_lib_hlsl=libspirv-cross-hlsl
+name_lib_msl=libspirv-cross-msl
+name_lib_reflect=libspirv-cross-reflect
+name_lib_util=libspirv-cross-util
+name_exe=spirv-cross
+
+if [ "$debug" == "debug" ]; then
+    name_project=$name"_d"
+    name_lib_core_final=$name_lib_core"_d.a"
+    name_lib_c_final=$name_lib_c"_d.a"
+    name_lib_cpp_final=$name_lib_cpp"_d.a"
+    name_lib_glsl_final=$name_lib_glsl"_d.a"
+    name_lib_hlsl_final=$name_lib_hlsl"_d.a"
+    name_lib_msl_final=$name_lib_msl"_d.a"
+    name_lib_reflect_final=$name_lib_reflect"_d.a"
+    name_lib_util_final=$name_lib_util"_d.a"
+    name_exe_final=$name_exe"_d"
+else
+    name_project=$name
+    name_lib_core_final=$name_lib_core".a"
+    name_lib_c_final=$name_lib_c".a"
+    name_lib_cpp_final=$name_lib_cpp".a"
+    name_lib_glsl_final=$name_lib_glsl".a"
+    name_lib_hlsl_final=$name_lib_hlsl".a"
+    name_lib_msl_final=$name_lib_msl".a"
+    name_lib_reflect_final=$name_lib_reflect".a"
+    name_lib_util_final=$name_lib_util".a"
+    name_exe_final=$name_exe
+fi
+
+if [ "$rebuild" == "rebuild" ]; then
+    rm -rf "../Build/MacOS/"$name_project
+fi
+mkdir -p "../Build/MacOS/"$name_project
+
+rm -rf "../Lib/MacOS/"$name_lib_core_final
+rm -rf "../Lib/MacOS/"$name_lib_c_final
+rm -rf "../Lib/MacOS/"$name_lib_cpp_final
+rm -rf "../Lib/MacOS/"$name_lib_glsl_final
+rm -rf "../Lib/MacOS/"$name_lib_hlsl_final
+rm -rf "../Lib/MacOS/"$name_lib_msl_final
+rm -rf "../Lib/MacOS/"$name_lib_reflect_final
+rm -rf "../Lib/MacOS/"$name_lib_util_final
+rm -rf "../../Bin/MacOS/"$name_exe_final
+mkdir -p "../Lib/MacOS"
+
+cd ..
+cd Build
+cd MacOS
+cd $name_project
+
+#lib
+if [ "$debug" == "debug" ]; then
+    cmake -DDEBUG=1 -DCMAKE_BUILD_TYPE=Debug -DSPIRV_CROSS_ENABLE_TESTS=OFF ../../../Sources/$name/
+else
+    cmake -DSPIRV_CROSS_ENABLE_TESTS=OFF ../../../Sources/$name/
+fi
+make
+
+cd ..
+cd ..
+cd ..
+cd Compile
+
+cp -rf "../Build/MacOS/"$name_project"/"$name_lib_core".a" "../Lib/MacOS/"$name_lib_core_final
+cp -rf "../Build/MacOS/"$name_project"/"$name_lib_c".a" "../Lib/MacOS/"$name_lib_c_final
+cp -rf "../Build/MacOS/"$name_project"/"$name_lib_cpp".a" "../Lib/MacOS/"$name_lib_cpp_final
+cp -rf "../Build/MacOS/"$name_project"/"$name_lib_glsl".a" "../Lib/MacOS/"$name_lib_glsl_final
+cp -rf "../Build/MacOS/"$name_project"/"$name_lib_hlsl".a" "../Lib/MacOS/"$name_lib_hlsl_final
+cp -rf "../Build/MacOS/"$name_project"/"$name_lib_msl".a" "../Lib/MacOS/"$name_lib_msl_final
+cp -rf "../Build/MacOS/"$name_project"/"$name_lib_reflect".a" "../Lib/MacOS/"$name_lib_reflect_final
+cp -rf "../Build/MacOS/"$name_project"/"$name_lib_util".a" "../Lib/MacOS/"$name_lib_util_final
+cp -rf "../Build/MacOS/"$name_project"/"$name_exe "../../Bin/MacOS/"$name_exe_final
+
+folderSrc="../Sources/$name"
+folderDst="../Include/MacOS/"$name
+rm -rf $folderDst
+mkdir -p $folderDst
+
+for file in ${folderSrc}/*.h
+do 
+    cp -rf $file $folderDst
+done
+for file in ${folderSrc}/*.hpp
+do 
+    cp -rf $file $folderDst
+done
---
+++ Dependencies/Compile/Build_Windows_lib_spirv-cross.bat
@@ -0,0 +1,116 @@
+@REM #****************************************************************************
+@REM # LostPeterVulkan - Copyright (C) 2022 by LostPeter
+@REM # 
+@REM # Author:   LostPeter
+@REM # Time:     2024-06-08
+@REM # Github:   https://github.com/LostPeter/LostPeterVulkan
+@REM # Document: https://www.zhihu.com/people/lostpeter/posts
+@REM #
+@REM # This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
+@REM #****************************************************************************/
+
+@echo off
+set debug=%1
+set rebuild=%2
+
+echo %debug%
+echo %rebuild%
+
+set name="spirv-cross-vulkan-sdk-1.3.283"
+
+set name_lib_core="spirv-cross-core"
+set name_lib_c="spirv-cross-c"
+set name_lib_cpp="spirv-cross-cpp"
+set name_lib_glsl="spirv-cross-glsl"
+set name_lib_hlsl="spirv-cross-hlsl"
+set name_lib_msl="spirv-cross-msl"
+set name_lib_reflect="spirv-cross-reflect"
+set name_lib_util="spirv-cross-util"
+set name_exe="spirv-cross"
+
+if "%debug%" == "debug" (
+    set name_project=%name%"_d"
+    set name_lib_core_final=%name_lib_core%"_d.lib"
+    set name_lib_c_final=%name_lib_c%"_d.lib"
+    set name_lib_cpp_final=%name_lib_cpp%"_d.lib"
+    set name_lib_glsl_final=%name_lib_glsl%"_d.lib"
+    set name_lib_hlsl_final=%name_lib_hlsl%"_d.lib"
+    set name_lib_msl_final=%name_lib_msl%"_d.lib"
+    set name_lib_reflect_final=%name_lib_reflect%"_d.lib"
+    set name_lib_util_final=%name_lib_util%"_d.lib"
+    set name_exe_final=%name_exe%"_d.exe"
+) else (
+    set name_project=%name%
+    set name_lib_core_final=%name_lib_core%".lib"
+    set name_lib_c_final=%name_lib_c%".lib"
+    set name_lib_cpp_final=%name_lib_cpp%".lib"
+    set name_lib_glsl_final=%name_lib_glsl%".lib"
+    set name_lib_hlsl_final=%name_lib_hlsl%".lib"
+    set name_lib_msl_final=%name_lib_msl%".lib"
+    set name_lib_reflect_final=%name_lib_reflect%".lib"
+    set name_lib_util_final=%name_lib_util%".lib"
+    set name_exe_final=%name_exe%".exe"
+)
+
+@rem build folder
+set build_folder="..\Build\Windows\"%name_project%
+if exist %build_folder% (
+    if "%rebuild%" == "rebuild" (
+        rmdir /S/Q %build_folder%
+    )
+)
+mkdir %build_folder%
+
+
+@rem Lib folder/file
+set plugins_folder="..\Lib\Windows"
+if not exist %plugins_folder% (
+    mkdir %plugins_folder%
+)
+
+
+cd ..
+cd Build
+cd Windows
+cd %name_project%
+
+if "%debug%" == "debug" (
+    cmake -DDEBUG=1 -DSPIRV_CROSS_ENABLE_TESTS=OFF "../../../Sources/%name%/"
+    msbuild spirv-cross.sln /p:configuration=debug
+    copy /Y ".\Debug\%name_lib_core%.lib" "..\..\..\Lib\Windows\"%name_lib_core_final%
+    copy /Y ".\Debug\%name_lib_c%.lib" "..\..\..\Lib\Windows\"%name_lib_c_final%
+    copy /Y ".\Debug\%name_lib_cpp%.lib" "..\..\..\Lib\Windows\"%name_lib_cpp_final%
+    copy /Y ".\Debug\%name_lib_glsl%.lib" "..\..\..\Lib\Windows\"%name_lib_glsl_final%
+    copy /Y ".\Debug\%name_lib_hlsl%.lib" "..\..\..\Lib\Windows\"%name_lib_hlsl_final%
+    copy /Y ".\Debug\%name_lib_msl%.lib" "..\..\..\Lib\Windows\"%name_lib_msl_final%
+    copy /Y ".\Debug\%name_lib_reflect%.lib" "..\..\..\Lib\Windows\"%name_lib_reflect_final%
+    copy /Y ".\Debug\%name_lib_util%.lib" "..\..\..\Lib\Windows\"%name_lib_util_final%
+    copy /Y ".\Debug\%name_exe%.exe" "..\..\..\..\Bin\Windows\"%name_exe_final%
+) else (
+    cmake -DSPIRV_CROSS_ENABLE_TESTS=OFF "../../../Sources/%name%/"
+    msbuild spirv-cross.sln /p:configuration=release
+    copy /Y ".\Release\%name_lib_core%.lib" "..\..\..\Lib\Windows\"%name_lib_core_final%
+    copy /Y ".\Release\%name_lib_c%.lib" "..\..\..\Lib\Windows\"%name_lib_c_final%
+    copy /Y ".\Release\%name_lib_cpp%.lib" "..\..\..\Lib\Windows\"%name_lib_cpp_final%
+    copy /Y ".\Release\%name_lib_glsl%.lib" "..\..\..\Lib\Windows\"%name_lib_glsl_final%
+    copy /Y ".\Release\%name_lib_hlsl%.lib" "..\..\..\Lib\Windows\"%name_lib_hlsl_final%
+    copy /Y ".\Release\%name_lib_msl%.lib" "..\..\..\Lib\Windows\"%name_lib_msl_final%
+    copy /Y ".\Release\%name_lib_reflect%.lib" "..\..\..\Lib\Windows\"%name_lib_reflect_final%
+    copy /Y ".\Release\%name_lib_util%.lib" "..\..\..\Lib\Windows\"%name_lib_util_final%
+    copy /Y ".\Release\%name_exe%.exe" "..\..\..\..\Bin\Windows\"%name_exe_final%
+)
+
+
+cd ..
+cd ..
+cd ..
+cd Compile
+
+set include_folder="..\Include\Windows\"%name%
+if exist %include_folder% (
+    rmdir /S/Q %include_folder%
+)
+mkdir %include_folder%
+
+xcopy /Y /F "..\Sources\%name%\*.h" %include_folder%"\"
+xcopy /Y /F "..\Sources\%name%\*.hpp" %include_folder%"\"
